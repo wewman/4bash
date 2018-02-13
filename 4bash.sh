929bc41c03380ccc163deea97a7f505512bb0e2f
813gan <29867887+813gan@users.noreply.github.com>
2018-02-13 17:21:19 +0100

    Add files via upload

--- 4bash.sh
+++ 4bash.sh
@@ -4,7 +4,8 @@
 ######### DEPENDANCIES #########
 ################################
 ##                            ##
-##            jq              ##
+##           jq               ##
+##           curl             ##
 ##           paste            ##
 ##                            ##
 ################################
@@ -16,18 +17,95 @@
 ##  To download:              ##
 ##     script.sh <url>        ##
 ##                            ##
+##  To download all threads   ##
+##  that match regrex:        ##
+##     script.sh -l\          ##
+##     <board> <regrex>       ##
+##                            ##
 ##  To clean:                 ##
 ##     script.sh <url> clean  ##
 ##                            ##
 ################################
 ################################
 
-## Get the URL via parameter 1
-url=$1
+## Set default values
+wgetargs=''
+quiet=false
+
+## Download all threads with subject or comment maching regular expression (PCRE, incasesensetive)
+lurkmode=false
+
+## Remove broken files
+cleanmode=false
+
+## Refresh time
+mins=1
+
+## Path to 4bash
+SCRIPT="$0"
+
+## Loop status
+loop=true
+
+
+## Parse commandline arguments
+while [[ $# -gt 0 ]]; do
+arg="$1"
+
+case $arg in
+    -q|--quiet)
+	wgetargs="$wgetargs -q"
+	quiet=true
+	shift
+	;;
+    -t|--refresh-time)
+	shift
+	mins="$1"
+	shift
+	;;
+    -l|--lurk)
+	lurkmode=true
+	shift
+	board="$1"
+	regrex="$2"
+	shift
+	shift
+	;;
+    -1|--once|once)
+	loop=false
+	shift
+	;;
+    -c|--clean|clean)
+	cleanmode=true
+	shift
+	;;
+    *)
+	url="$1"
+	shift
+	;; 
+esac
+done
+
+if $lurkmode ; then
+    args=''
+    [[ ! loop ]] && args='--once' 
+    for no in $(curl "a.4cdn.org/$board/catalog.json" -s |\
+		     jq --arg regrex "$regrex" '.[] | .threads | .[] | if (.com + "\n" + .sub | test( $regrex;"i" )) then .no  else empty end? ')
+    do "$SCRIPT" --quiet $args --refresh-time "$mins" "https://boards.4chan.org/$board/thread/$no" &
+    done
+
+    exit
+
+fi
+
+
+## Sanitize URL
+url=$(echo $url | sed -r 's!^https?://!!' | sed 's/#.*//')
 ## Get the board name
-board=$(echo $url | awk -F"/" '{print $4}')
+board=$(echo $url | awk -F"/" '{print $2}')
 ## Get the thread number
-thread=$(echo $url | awk -F"/" '{print $6}')
+thread=$(echo $url | awk -F"/" '{print $4}')
+
 
 ## Dir maker ##
 mkdir -p ./$board
@@ -36,76 +114,62 @@ mkdir -p ./$board/$thread
 ## Dir location
 dir=./$board/$thread
 
-## Refresh time
-secs=$((1 * 60))
+## Refresh time in seconds
+secs=$(($mins * 60))
 
-## Loop status
-loop=true
 
-## Note, second parameter is only to clean.
-#   if the second parameter is anyting else
-#   it will skip and go to the loop
-case $2 in
+if $cleanmode ; then
+## If clean, check for file health
+#   I know the method is cheesy, but it works
+#   TODO Optimize these loops into one?
+    echo Checking for Broken JPGs
+    for image in $dir/*.jpg; do
+	eof=$(xxd -s -0x04 $image | awk '{print $3}')
+	if [[ ! "$eof" = "ffd9" ]]; then
+	    echo Removing $image
+	    rm $image
+	fi
+    done
 
-    ## If clean, check for file health
-    #   I know the method is cheesy, but it works
-    #   TODO Optimize these loops into one?
-    clean)
-        echo Checking for Broken JPGs
-        for image in $dir/*.jpg; do
-            eof=$(xxd -s -0x04 $image | awk '{print $3}')
-            if [[ ! "$eof" = "ffd9" ]]; then
-                echo Removing $image
-                rm $image
-            fi
-        done
+    echo Checking for Broken PNGs
+    for image in $dir/*.png; do
+	eof=$(xxd -s -0x04 $image | awk '{print $2 $3}')
+	if [[ ! "$eof" = "ae426082" ]]; then
+	    echo Removing $image
+	    rm $image
+	fi
+    done
 
-        echo Checking for Broken PNGs
-        for image in $dir/*.png; do
-            eof=$(xxd -s -0x04 $image | awk '{print $2 $3}')
-            if [[ ! "$eof" = "ae426082" ]]; then
-                echo Removing $image
-                rm $image
-            fi
-        done
+    echo Checking for Broken GIFs
+    for image in $dir/*.gif; do
+	eof=$(xxd -s -0x04 $image | awk '{print $3}')
+	if [[ ! "$eof" = "003b" ]]; then
+	    echo Removing $image
+	    rm $image
+	fi
+    done
 
-        echo Checking for Broken GIFs
-        for image in $dir/*.gif; do
-            eof=$(xxd -s -0x04 $image | awk '{print $3}')
-            if [[ ! "$eof" = "003b" ]]; then
-                echo Removing $image
-                rm $image
-            fi
-        done
+    echo Checking for Broken WEBMs
+    for image in $dir/*.webm; do
+	eof=$(xxd -s -0x04 $image | awk '{print $3}')
+	if [[ ! "$eof" = "8104" ]]; then
+	    echo Removing $image
+	    rm $image
+	fi
+    done
 
-        echo Checking for Broken WEBMs
-        for image in $dir/*.webm; do
-            eof=$(xxd -s -0x04 $image | awk '{print $3}')
-            if [[ ! "$eof" = "8104" ]]; then
-                echo Removing $image
-                rm $image
-            fi
-        done
+    # I set it to exit the script after cleaning
+    # But it would be nice too if you can make it
+    # Run the script afterwards, but whatever
+    exit
+fi
 
-        # I set it to exit the script after cleaning
-        # But it would be nice too if you can make it
-        # Run the script afterwards, but whatever
-        exit
-        ;;
-
-    once)
-        loop=false
-        continue
-        ;;
-
-    ## If nothing is set, keep going
-    *)
-esac
+echo "Downloading thread $board/$thread"
 
 ## Loop forever until ^C or exit
 while true; do
 
-    echo Updating file...
+    [[ $quiet ]] || echo Updating file...
 
     ## This will get the JSON from a.4cdn.org
     #   output it to a file quietly to save space
@@ -113,7 +177,7 @@ while true; do
     #
     #   NOTE that it will download this every time and replace
     #     the one you have regardless if it's old or the same
-    wget -N https://a.4cdn.org/$board/thread/$thread.json -O $dir/$thread.json --quiet || { echo 'Thread deleted or does not exist.'; exit; } 
+    wget -N https://a.4cdn.org/$board/thread/$thread.json -O $dir/$thread.json --quiet || { echo "Thread $board/$thread deleted or does not exist."; exit; } 
 
     ## This will interpret the json file and get only the `tim` and `ext`
     #   Then save it to a file so wget can use `-i` to download everything
@@ -133,7 +197,7 @@ while true; do
     #   will just remove the whole wget output text
     #  TODO Make it so it does not output any messages when
     #   the files exist.
-    wget -nc -P $dir/ -c -i $dir/$thread.files --progress=dot
+    wget ${wgetargs} -nc -P $dir/ -c -i $dir/$thread.files --progress=dot
 
     ## Exit if requested to run once.
     if ! $loop ; then
@@ -147,8 +211,10 @@ while true; do
     #   but this does the job anyway
     sec=$secs
     while [ $sec -gt 0 ]; do
-        printf "Download complete. Refreshing in:  %02d\033[K\r" $sec
-        sleep 1
+	if ! $quiet ; then
+            printf "Download complete. Refreshing in:  %02d\033[K\r" $sec
+	fi
+	sleep 1
         : $((sec--))
     done
 
